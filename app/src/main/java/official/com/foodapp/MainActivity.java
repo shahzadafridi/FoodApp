23e1569a211803453d7c6fb1f3bf0ab8c402f4dd
shahzad afridi <shahzadahmadafridi@gmail.com>
2019-09-04 16:08:03 +0500

    Activity LifeCycle explained.

--- app/src/main/java/official/com/foodapp/MainActivity.java
+++ app/src/main/java/official/com/foodapp/MainActivity.java
@@ -1,6 +1,7 @@
 package official.com.foodapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,12 +16,13 @@ public class MainActivity extends AppCompatActivity{
     EditText username,password;
     Button login;
     TextView textView;
+    String TAG = "MainActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linear_layout);
-
+        Log.e(TAG, "onCreate");
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
@@ -31,12 +33,46 @@ public class MainActivity extends AppCompatActivity{
                 String str_usernam = username.getText().toString();
                 String str_password = password.getText().toString();
                 if (str_usernam.equals("admin") && str_password.equals("admin")){
-                    textView.setText("User succesfully login in");
+                    textView.setText("User succesfully login in.");
+
+                    // store locally these value
+                    // username, id, is_login = ture;
                 }else {
                     textView.setText("Invalid credential.");
                 }
             }
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart");
+        // is_login = true go to main activity.
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy");
     }
 }
